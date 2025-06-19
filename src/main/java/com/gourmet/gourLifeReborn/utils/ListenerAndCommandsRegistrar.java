e08c9654e81823e97b01dcc7b51fc7ac6c723b08
gourmet <insogna_@outlook.it>
2025-06-19 18:50:44 +0200

    Added register command

--- src/main/java/com/gourmet/gourLifeReborn/utils/ListenerAndCommandsRegistrar.java
+++ src/main/java/com/gourmet/gourLifeReborn/utils/ListenerAndCommandsRegistrar.java
@@ -1,18 +1,35 @@
 package com.gourmet.gourLifeReborn.utils;
 
 import com.gourmet.gourLifeReborn.GourLifeReborn;
+import com.gourmet.gourLifeReborn.commands.LifeCMD;
 import com.gourmet.gourLifeReborn.events.KillEvent;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
+import revxrsal.commands.Lamp;
+import revxrsal.commands.bukkit.BukkitLamp;
+import revxrsal.commands.bukkit.actor.BukkitCommandActor;
 
 import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ListenerRegistrar {
+public class ListenerAndCommandsRegistrar {
 
     public static void registerAll(){
+        registerEvents();
+        registerCommands();
+    }
+
+    private static void registerCommands(){
+        Lamp<BukkitCommandActor> handler = BukkitLamp.builder(GourLifeReborn.getInstance()).build();
+
+        handler.register(
+                new LifeCMD()
+        );
+    }
+
+    private static void registerEvents(){
         PluginManager pm = GourLifeReborn.getInstance().getServer().getPluginManager();
 
         List<Listener> listeners = List.of(
