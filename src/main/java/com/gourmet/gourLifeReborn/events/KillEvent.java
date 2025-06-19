d00e189c9d515da193a548f255080db530bbd9ef
gourmet <insogna_@outlook.it>
2025-06-19 18:50:11 +0200

    Added kill event system

--- src/main/java/com/gourmet/gourLifeReborn/events/KillEvent.java
+++ src/main/java/com/gourmet/gourLifeReborn/events/KillEvent.java
@@ -1,6 +1,8 @@
 package com.gourmet.gourLifeReborn.events;
 
 import com.gourmet.gourLifeReborn.database.DatabaseMySQL;
+import com.gourmet.gourLifeReborn.utils.Utils;
+import com.gourmet.gourLifeReborn.utils.config.LanguageConfigManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -9,15 +11,52 @@ public class KillEvent implements Listener {
 
     @EventHandler
     public void onKill(PlayerDeathEvent e){
+        var databaseManager = DatabaseMySQL.getInstance();
+        var lang = LanguageConfigManager.getInstance();
         var player = e.getPlayer();
         var killer = player.getKiller();
 
-        if(killer == null) return;
+        e.setDeathMessage("");
 
-        var databaseManager = DatabaseMySQL.getInstance();
+        if (killer == null) {
+            databaseManager.removeLife(player).thenCompose(v ->
+                    databaseManager.getLife(player)
+            ).thenAccept(newLives -> {
+                var message = lang.deathMessage
+                        .replace("%player%", player.getName())
+                        .replace("%lives%", String.valueOf(newLives));
 
-        databaseManager.addLife(killer);
-        databaseManager.removeLife(player);
+                Utils.sendMessageAll(message);
+
+                if (newLives <= 0) {
+                    finalDeath();
+                }
+            });
+        } else {
+
+            databaseManager.removeLife(player).thenCompose(v ->
+                    databaseManager.addLife(killer)
+            ).thenCompose(v ->
+                    databaseManager.getLife(player)
+            ).thenAccept(newLives -> {
+                var message = lang.deathByPlayerMessage
+                        .replace("%player%", player.getName())
+                        .replace("%lives%", String.valueOf(newLives))
+                        .replace("%killer%", killer.getName());
+
+                Utils.sendMessageAll(message);
+
+                if (newLives <= 0) {
+                    finalDeath();
+                }
+            });
+
+        }
+
+    }
+
+    // TODO: ban o final kill
+    private void finalDeath(){
 
     }
 
